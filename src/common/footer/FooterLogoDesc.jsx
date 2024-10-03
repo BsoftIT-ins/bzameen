d8c49d1eba395ef0d832ed41d4bdf99da6ab6fdd
Azizul Islam <bsoftitinfo@gmail.com>
2024-10-03 11:23:13 +0600

    real estate setup

---
+++ src/common/footer/FooterLogoDesc.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import LogoWhite from '../LogoWhite';
+
+const FooterLogoDesc = () => {
+    return (
+        <>
+            <div className="footer-item__logo">
+                <LogoWhite/>
+            </div>
+            <p className="footer-item__desc">Bzomeen.com is a pioneer Real Estate marketplace in Dhaka, Bangladesh</p>
+                
+        </>
+    );
+};
+
+export default FooterLogoDesc;
